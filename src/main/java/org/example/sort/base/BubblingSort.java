c46166d58b9173fe9b09686e58029b53186f1882
luoyu <18715838915@163.com>
2023-11-14 21:34:56 +0800

    冒泡排序

--- src/main/java/org/example/sort/base/BubblingSort.java
+++ src/main/java/org/example/sort/base/BubblingSort.java
@@ -6,6 +6,59 @@ package org.example.sort.base;
 public class BubblingSort {
 
 
+    public static void main(String[] args) {
+
+        int [] array = {2,5,22,90,1,20};
+
+        preSort(array);
+
+
+        System.out.println(array);
+
+
+    }
+
+
+
+    /**
+     * 从前面开始冒泡排序
+     * @param array
+     */
+    public static void preSort(int [] array) {
+        if(array == null || array.length < 2) {
+            return;
+        }
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - i; j++) {
+                int left= array[j];
+                int nextIndex = j + 1;
+                if(nextIndex >= array.length) {
+                    break;
+                }
+                int right = array[nextIndex];
+                if(left > right) {
+                    array[j] = right;
+                    array[nextIndex] = left;
+                }
+            }
+        }
+        
+        
+        
+        
+        
+
+
+        
+
+
+
+
+
+
+
+    }
+
 
 
 
